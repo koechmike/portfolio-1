d72fbc26700e7ddf9f20ba1be930cfd7521a49b5
AnmolPandita-sudo <82043884+AnmolPandita-sudo@users.noreply.github.com>
2024-01-23 16:15:12 +0530

    Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export default function Header({ socials }: Props) {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between w-full rounded-full mx-auto z-20 xl:items-center bg-[#3b3b3b]">
+    <header className="sticky top-0 p-5 flex items-start justify-between w-full rounded-full mx-auto z-20 xl:items-center">
       <motion.div
         initial={{
           x: -500,
